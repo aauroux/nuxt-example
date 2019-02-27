10253e29d8d9e520d8d55473eee49f26b28444a9
Adrien Auroux <adrien.auroux@gmail.com>
2019-02-27 10:33:31 +0100

    Cleanup

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,8 +41,6 @@ module.exports = {
                 rel: 'stylesheet',
                 type: 'text/css',
                 href: 'https://use.fontawesome.com/releases/v5.7.1/css/all.css',
-                integrity: 'sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr',
-                crossorigin: 'anonymous',
             },
         ],
     },
